7adc152b9382abfddb8bd008511fb3666ac78706
Lauren Savage <17114944+savagedata@users.noreply.github.com>
2022-09-22 21:39:52 -0500

    Create game manager

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 
 import yaml
+import random
 
 def main():
-    suspects = get_suspects()
+    game_manager = GameManager(1)
+    print(game_manager.suspects)
 
-    print(suspects)
+class GameManager():
+    def __init__(self, seed: int = None):
+        self.reset(seed)
+
+    def reset(self, seed: int = None):
+        self.seed = seed or random.random()
+        random.seed(self.seed)
+        self.suspects = self.get_suspects()
+
+    def get_suspects(self):
+        print("Retrieving suspects")
+        with open("configs/suspects.yml", "r") as stream:
+            try:
+                return yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+                return None
+    
+    def generate_evidence(self):
+        return
 
 
-
-def get_suspects():
-    with open("configs/suspects.yml", "r") as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            return None
-
-
-
-main()
+if __name__ == "__main__":
+    main()
