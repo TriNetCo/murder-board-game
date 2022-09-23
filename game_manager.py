6a6fdacb749f6d14d63ae0d25ec1dadb69db3016
Lauren Savage <17114944+savagedata@users.noreply.github.com>
2022-09-22 23:30:28 -0500

    fleshing out presentation

--- game_manager.py
+++ game_manager.py
@@ -8,11 +8,19 @@ class GameManager():
     def reset(self, seed: int = None):
         self.seed = seed or random.random()
         random.seed(self.seed)
-        self.suspects = self._get_suspects()
+        self.suspects = self._load_config("suspects")
+        self.weapons = self._load_config("weapons")
         self._generate_evidence()
 
-    def _get_suspects(self):
-        print("Retrieving suspects")
+    def _load_config(self, name):
+        with open("configs/{}.yml".format(name), "r") as stream:
+            try:
+                return yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+                return None
+
+    def _load_suspects(self):
         with open("configs/suspects.yml", "r") as stream:
             try:
                 return yaml.safe_load(stream)
@@ -20,5 +28,45 @@ class GameManager():
                 print(exc)
                 return None
     
+    def get_suspect_names(self):
+        return '\n'.join([x['display_name'] for x in self.suspects])
+
+    def get_attributes(self, domain_name, property_name):
+        domain = getattr(self, domain_name)
+        properties = [x[property_name] for x in domain]
+        properties_str = ', '.join(properties)
+        return properties
+        #return print("{} {}s : {}".format(domain_name, property_name, properties_str))
+
+    def present_suspects(self):
+        yaml_name = 'suspects'
+        items = self.get_attributes(yaml_name, 'display_name')
+        print("Suspects: ")
+        for item in items:
+            print("  " + item)
+
+    def present_weapons(self):
+        yaml_name = 'weapons'
+        items = self.get_attributes(yaml_name, 'display_name')
+        print("Weapons: ")
+        for item in items:
+            print("  " + item)
+
+        # print("Suspects:\n - ", "\n - ".join(self.get_attributes('suspects', 'display_name')))
+
+
+
+    """
+    def __getattr__(self, name):
+        def _missing(*args, **kwargs):
+            # name = weapon_name
+            return self.get_the_names(name)
+        return _missing
+    """
     def _generate_evidence(self):
+        suspects = self.suspects
+        weapons = self.weapons
+        random_list = random.sample(range(len(suspects)), len(suspects))
+        for i in range(len(suspects)):
+            suspects[i]['weapon'] = weapons[random_list[i]]
         return
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from game_manager import GameManager
 
 def main():
-    game_manager = GameManager(1)
-    print(game_manager.suspects)
+    print()
+
+    game = GameManager(1)
+    # print(game.get_suspect_names())
+
+    game.present_suspects()
+    print()
+    game.present_weapons()
+
+    # print(game.get_suspect_names())
+    # print(game.get_weapon_names())
+    # print(game.get_weapon_names())
+    # print(game.weapons)
+
 
 if __name__ == "__main__":
     main()
