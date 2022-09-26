afef02067e2107a3f7aa1b3e5d3d41fe6d872677
Lauren Savage <17114944+savagedata@users.noreply.github.com>
2022-09-25 21:38:05 -0500

    Add pretty print method

--- debug_mixin.py
+++ debug_mixin.py
@@ -1,5 +1,10 @@
+import yaml
+
 class DebugMixin:
     def __repr__(self):
         class_name = self.__class__.__name__
         attributes = str(self.__dict__)
-        return class_name + ": " + attributes
+        return "{'" + class_name + "': " + attributes + "}"
+
+    def pretty_print(self):
+        print(yaml.dump(self))
