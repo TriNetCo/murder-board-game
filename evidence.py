17af4a67632e8a65a5612485277a6c734053dce7
Lauren Savage <17114944+savagedata@users.noreply.github.com>
2022-09-26 20:58:00 -0700

    Fix evidence type to not be None

--- evidence.py
+++ evidence.py
@@ -3,7 +3,7 @@ from debug_mixin import DebugMixin
 class Evidence(DebugMixin):
     def __init__(self, display_name: str, type: str = None):
         self.display_name = display_name
-        self.type = None
+        self.type = type
 
 class SuspectEvidence(DebugMixin):
     def __init__(self):
