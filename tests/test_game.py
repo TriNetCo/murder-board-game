4324b5c1949b1362049a7d332e956f010fb82cde
Lauren Savage <17114944+savagedata@users.noreply.github.com>
2022-09-29 17:59:13 -0700

    Test for suspect names

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,3 +1,4 @@
+# run pytest tests/test_game.py
 import pytest
 from murder_board_game.game import Game
 from murder_board_game.config import load_entities_from_config
@@ -5,4 +6,7 @@ from murder_board_game.config import load_entities_from_config
 def test_load_entities_from_config():
     game = Game()
     load_entities_from_config(game)
-
+    suspect_display_names = [suspect.display_name for suspect in game.suspects_pool]
+    for suspect in ['Gangster', 'Outlaw', 'Kona', 'Bartender']:
+        fail_message = "{} not in suspect display names: {}".format(suspect, str(suspect_display_names))
+        assert suspect in suspect_display_names, fail_message
