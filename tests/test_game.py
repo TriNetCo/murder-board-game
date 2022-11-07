b0a6eed23f8b2d96069832aa833b15308896ecf4
Lauren Savage <17114944+savagedata@users.noreply.github.com>
2022-11-06 18:56:43 -0600

    fixes test naming

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,9 +1,9 @@
-# run pytest tests/game_test.py
+# run pytest tests/test_game.py
 import pytest
 from murder_board_game.game import Game
 from murder_board_game.config import load_entities_from_config
 
-def load_entities_from_config_test():
+def test_load_entities_from_config_test():
     game = Game()
     load_entities_from_config(game)
     suspect_display_names = [suspect.display_name for suspect in game.suspects_pool]
